4bef357d693ddfae76e421f01ad2e5ba5a039d80
Alex <alejandroherpal@gmail.com>
2022-02-28 13:44:48 +0100

    fin seccion 3

--- adminpro/src/app/auth/login/login.component.ts
+++ adminpro/src/app/auth/login/login.component.ts
@@ -2,9 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styles: [
-  ]
+  templateUrl: './login.component.html',  
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
