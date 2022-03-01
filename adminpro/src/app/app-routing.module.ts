8666a63df1c2e706b0b08659dca39e937877f335
Alex <alejandroherpal@gmail.com>
2022-03-01 16:50:32 +0100

    Fin seccion 4 - Rutas

--- adminpro/src/app/app-routing.module.ts
+++ adminpro/src/app/app-routing.module.ts
@@ -1,37 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { PagesComponent } from './pages/pages.component';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+
 
 const routes: Routes = [
-  //Rutas protegidas
-  {
-    path: '', 
-    component: PagesComponent,
-    children: [
-      {path: 'dashboard', component: DashboardComponent},
-      {path: 'progress', component: ProgressComponent},
-      {path: 'grafica1', component: Grafica1Component}
-    ]
-  },
-  
 
-  //Rutas publics
-  {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent},
-  
-  //{path: '', redirectTo: '/dashboard', pathMatch: 'full'},//Para parth vacio
+  /************************************************
+   * RELACION DE RUTAS
+   * **********************************************
+   * PagesRouting:
+   *   -> /dashboard
+   *   -> /grafica1
+   *   -> /progress
+   * 
+   * AuthRouting:
+   *   -> /login
+   *   -> /register
+   * *********************************************
+   */
+
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},//Para parth vacio
   {path: '**', component: NopagefoundComponent}//Para url's que no se correspondan con ninguna
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],//forRoot es para especificar las rutas principales (padres) y le decimos que son las que estan
-  //almacenadas en la constante routes
+  imports: [
+    RouterModule.forRoot(routes),//forRoot es para especificar las rutas principales (padres) y le decimos que son las que estan
+    //almacenadas en la constante routes
+    PagesRoutingModule,
+    AuthRoutingModule
+  ],
+  
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
